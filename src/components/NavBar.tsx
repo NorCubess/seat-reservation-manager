b80c1e28d6ea0d36ed9640680a6200257d327886
NorCubes <ronnicolastan@gmail.com>
2025-12-01 16:24:55 +0800

    [component] Update NavBar and WordButton Code

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Leckerli_One } from "next/font/google";
 import WordButton from "./WordButton";
+import SolidButton from "./SolidButton";
 
 const leckerli = Leckerli_One({ subsets: ["latin"], weight: "400" });
 
 export default function NavBar() {
   return (
-    <nav style={{ backgroundColor: "#0E271B" }} className="relative w-full">
+    <nav
+      style={{ backgroundColor: "#0E271B" }}
+      className="relative w-full flex items-center justify-between"
+    >
       {/* left logo area */}
       <div
-        className="w-full py-4 flex items-center"
+        className="py-4 flex items-center"
         style={{ paddingLeft: "50px", paddingRight: "12px" }}
       >
         <span className={`${leckerli.className} text-white text-2xl`}>
@@ -17,12 +21,17 @@ export default function NavBar() {
         </span>
       </div>
 
-      {/* centered buttons (absolute center) */}
+      {/* centered buttons */}
       <div className="absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 flex gap-3">
         <WordButton>Placeholder</WordButton>
         <WordButton>Placeholder</WordButton>
         <WordButton>Placeholder</WordButton>
         <WordButton>Placeholder</WordButton>
+      </div>
+
+      {/* right login button */}
+      <div className="py-4 flex items-center" style={{ paddingRight: "50px" }}>
+        <SolidButton>Login</SolidButton>
       </div>
     </nav>
   );
